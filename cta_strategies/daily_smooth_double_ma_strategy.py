f256b58dd4dfa03129d84ae8d6e5013129b9381e
qiyuexuezainanfang <47418093+qiyuexuezainanfang@users.noreply.github.com>
2022-11-07 21:21:44 +0800

    [Add]日线和分钟线的平滑版本双均线策略

---
+++ cta_strategies/daily_smooth_double_ma_strategy.py
@@ -0,0 +1,257 @@
+from vnpy_ctastrategy import (
+    CtaTemplate,
+    StopOrder,
+    TickData,
+    BarData,
+    TradeData,
+    OrderData,
+    BarGenerator,
+    ArrayManager,
+)
+from vnpy.trader.constant import Interval
+from datetime import datetime
+from typing import Callable
+
+
+class DailySmoothDoubleMaStrategy(CtaTemplate):
+    author = "用Python的交易员"
+
+    fast_window = 3
+    slow_window = 50
+    diff_window = 30
+
+    fast_ma0 = 0.0
+    fast_ma1 = 0.0
+
+    slow_ma0 = 0.0
+    slow_ma1 = 0.0
+
+    diff_ma = 0.0
+
+    parameters = ["fast_window", "slow_window", "diff_window"]
+    variables = ["fast_ma0", "fast_ma1", "slow_ma0", "slow_ma1", "diff_ma"]
+
+    def __init__(self, cta_engine, strategy_name, vt_symbol, setting):
+        """"""
+        super().__init__(cta_engine, strategy_name, vt_symbol, setting)
+
+        self.bg = DailyBarGenerator(self.on_bar, 1, self.on_daily_bar)
+        self.am = ArrayManager()
+
+    def on_init(self):
+        """
+        Callback when strategy is inited.
+        """
+        self.write_log("策略初始化")
+        self.load_bar(30)
+
+    def on_start(self):
+        """
+        Callback when strategy is started.
+        """
+        self.write_log("策略启动")
+        self.put_event()
+
+    def on_stop(self):
+        """
+        Callback when strategy is stopped.
+        """
+        self.write_log("策略停止")
+
+        self.put_event()
+
+    def on_tick(self, tick: TickData):
+        """
+        Callback of new tick data update.
+        """
+        self.bg.update_tick(tick)
+
+    def on_bar(self, bar: BarData):
+        self.bg.update_bar(bar)
+
+    def on_daily_bar(self, bar: BarData):
+        """
+        Callback of new bar data update.
+        """
+
+        am = self.am
+        am.update_bar(bar)
+        if not am.inited:
+            return
+
+        fast_ma = am.sma(self.fast_window, array=True)
+        self.fast_ma0 = fast_ma[-1]
+        self.fast_ma1 = fast_ma[-2]
+
+        slow_ma = am.ema(self.slow_window, array=True)
+        self.slow_ma0 = slow_ma[-1]
+        self.slow_ma1 = slow_ma[-2]
+
+        diff = fast_ma - slow_ma
+        diff_mean_now = sum(diff[-self.diff_window:])/len(diff[-self.diff_window:])
+        diff_mean_past = sum(diff[-self.diff_window-1:-1])/len(diff[-self.diff_window-1:-1])
+
+        cross_over = diff_mean_now > 0 and diff_mean_past < 0#上穿0
+        cross_below = diff_mean_now < 0 and diff_mean_past > 0#下穿0
+
+        if cross_over:
+            if self.pos == 0:
+                self.buy(bar.close_price, 1)
+            elif self.pos < 0:
+                self.cover(bar.close_price, 1)
+                self.buy(bar.close_price, 1)
+
+        elif cross_below:
+            if self.pos == 0:
+                self.short(bar.close_price, 1)
+            elif self.pos > 0:
+                self.sell(bar.close_price, 1)
+                self.short(bar.close_price, 1)
+
+        self.put_event()
+
+    def on_order(self, order: OrderData):
+        """
+        Callback of new order data update.
+        """
+        pass
+
+    def on_trade(self, trade: TradeData):
+        """
+        Callback of new trade data update.
+        """
+        self.put_event()
+
+    def on_stop_order(self, stop_order: StopOrder):
+        """
+        Callback of stop order update.
+        """
+        pass
+
+
+class DailyBarGenerator(BarGenerator):
+    """生成日K线"""
+
+    def __init__(
+        self,
+        on_bar: Callable,
+        window: int = 0,
+        on_window_bar: Callable = None,
+        interval: Interval = Interval.MINUTE
+    ) -> None:
+        """Constructor"""
+        self.bar: BarData = None
+        self.on_bar: Callable = on_bar
+
+        self.interval: Interval = interval
+        self.interval_count: int = 0
+
+        self.hour_bar: BarData = None
+        self.daily_bar: BarData = None
+
+        self.window: int = window
+        self.window_bar: BarData = None
+        self.on_window_bar: Callable = on_window_bar
+
+        self.last_tick: TickData = None
+
+    def update_bar(self, bar: BarData) -> None:
+        """
+        Update new tick data into generator.
+        """
+        self.update_bar_daily_window(bar)
+
+    def update_bar_daily_window(self, bar: BarData) -> None:
+        # If not inited, create window bar object
+        if not self.daily_bar:
+            dt: datetime = bar.datetime.replace(hour=0, minute=0, second=0, microsecond=0)
+            self.daily_bar = BarData(
+                symbol=bar.symbol,
+                exchange=bar.exchange,
+                datetime=dt,
+                gateway_name=bar.gateway_name,
+                open_price=bar.open_price,
+                high_price=bar.high_price,
+                low_price=bar.low_price,
+                close_price=bar.close_price,
+                volume=bar.volume,
+                turnover=bar.turnover,
+                open_interest=bar.open_interest
+            )
+            return
+
+        finished_bar: BarData = None
+
+
+        # If minute bar of new day, then push existing window bar
+        if bar.datetime.day != self.daily_bar.datetime.day:
+            finished_bar = self.daily_bar
+            dt: datetime = bar.datetime.replace(hour=0, minute=0, second=0, microsecond=0)
+            self.daily_bar = BarData(
+                symbol=bar.symbol,
+                exchange=bar.exchange,
+                datetime=dt,
+                gateway_name=bar.gateway_name,
+                open_price=bar.open_price,
+                high_price=bar.high_price,
+                low_price=bar.low_price,
+                close_price=bar.close_price,
+                volume=bar.volume,
+                turnover=bar.turnover,
+                open_interest=bar.open_interest
+            )
+        # Otherwise only update minute bar
+        else:
+            self.daily_bar.high_price = max(
+                self.daily_bar.high_price,
+                bar.high_price
+            )
+            self.daily_bar.low_price = min(
+                self.daily_bar.low_price,
+                bar.low_price
+            )
+
+            self.daily_bar.close_price = bar.close_price
+            self.daily_bar.volume += bar.volume
+            self.daily_bar.turnover += bar.turnover
+            self.daily_bar.open_interest = bar.open_interest
+
+        # Push finished window bar
+        if finished_bar:
+            self.on_daily_bar(finished_bar)
+
+    def on_daily_bar(self, bar: BarData) -> None:
+        """"""
+        if self.window == 1:
+            self.on_window_bar(bar)
+        else:
+            if not self.window_bar:
+                self.window_bar = BarData(
+                    symbol=bar.symbol,
+                    exchange=bar.exchange,
+                    datetime=bar.datetime,
+                    gateway_name=bar.gateway_name,
+                    open_price=bar.open_price,
+                    high_price=bar.high_price,
+                    low_price=bar.low_price
+                )
+            else:
+                self.window_bar.high_price = max(
+                    self.window_bar.high_price,
+                    bar.high_price
+                )
+                self.window_bar.low_price = min(
+                    self.window_bar.low_price,
+                    bar.low_price
+                )
+
+            self.window_bar.close_price = bar.close_price
+            self.window_bar.volume += bar.volume
+            self.window_bar.turnover += bar.turnover
+            self.window_bar.open_interest = bar.open_interest
+
+            self.interval_count += 1
+            if not self.interval_count % self.window:
+                self.interval_count = 0
+                self.on_window_bar(self.window_bar)
+                self.window_bar = None
